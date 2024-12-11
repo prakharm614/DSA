8f0ca6ff1d8cfb0f9469c2c15270b56f26b05f3d
Prakhar Mishra <prakharmishra614@gmail.com>
2024-12-11 12:57:02 +0530

    swap-n-sort

--- GFG/27-merge-sorted-wo-extrasp.cpp
+++ GFG/27-merge-sorted-wo-extrasp.cpp
@@ -1,3 +1,11 @@
+// C++ program to merge two sorted arrays a[] and b[] 
+// without extra space Using swap and sort
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void mergeArrays(vector<int>& a, vector<int>& b) {
     int i = a.size() - 1, j = 0;
 
       // swap smaller elements from b[] 
@@ -9,3 +17,17 @@
     // Sort both arrays
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
+}
+
+int main() {
+    vector<int> a = {1, 5, 9, 10, 15, 20};
+    vector<int> b = {2, 3, 8, 13};
+    mergeArrays(a, b);
+
+    for (int ele: a)
+        cout << ele << " ";
+    cout << "\n";
+    for (int ele: b)
+        cout << ele << " ";
+    return 0;
+}
